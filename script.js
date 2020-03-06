a835f31828d2cbf19cfb5086f8520157e20e4bac
Isa Griese <isagriese@gmail.com>
2020-03-06 10:57:54 +0100

    Hollys Books

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("about").onclick = function() {showAbout()};
+function showAbout() {
+    document.getElementById("about").innerHTML = "is a London based poet, from Ōtepoti /Dunedin, New Zealand. She graduated with a BA in Creative Writing from Goldsmiths University, London. Holly's work examines the unheimlich and embraces her dyslexia. She has slithered through haunted houses and mounted many buses to bring you these works.<br><span>Photo: Ting Cheng</span><img src=\"http://hollyfletcher.org/wp-content/uploads/2020/03/hollyfletcher-by-tingcheng.jpg\" width=\"100%\">";
+}
